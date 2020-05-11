6a5d327c31363896bf4f88cbd624f634390e4d5d
Vova <bobturanov@gmail.com>
2020-05-11 23:55:37 +0300

    ready to deploy

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def render_server_error(error):
     return "Что-то не так, но мы все починим:\n{}".format(error.original_exception), 500
 
 
-app.run()  # запустим сервер
+if __name__ == '__main__':
+    app.run()
